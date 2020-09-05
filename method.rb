b3035876911d3028ddf61c358e3ce583848c8685
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-09-05 05:52:20 +0000

    [add]method to get price

--- method.rb
+++ method.rb
@@ -3,15 +3,18 @@ require 'uri'
 require 'json'
 require './key.rb'
 
-# 板情報を取得
-uri = URI.parse("https://api.bitflyer.com")
-uri.path = '/v1/getboard'
-uri.query = ''
+# 現在の価格を取得
+def get_price
+	# 板情報を取得
+	uri = URI.parse("https://api.bitflyer.com")
+	uri.path = '/v1/getboard'
+	uri.query = ''
 
-# データを取得
-https = Net::HTTP.new(uri.host, uri.port)
-https.use_ssl = true
-response = https.get uri.request_uri
+	# データを取得
+	https = Net::HTTP.new(uri.host, uri.port)
+	https.use_ssl = true
+	response = https.get uri.request_uri
 
-response_hash = JSON.parse(response.body)
-puts response_hash["mid_price"]
+	response_hash = JSON.parse(response.body)
+	response_hash["mid_price"]
+end
