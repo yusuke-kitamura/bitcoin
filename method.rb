a77c3f1dd01a7c6dd394657f3d802d0aeded77b5
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-09-05 06:04:24 +0000

    [update]price to variable

--- method.rb
+++ method.rb
@@ -19,3 +19,39 @@ def get_price
 	response_hash = JSON.parse(response.body)
 	response_hash["mid_price"]
 end
+
+def buy_order(price)
+	key = API_KEY
+	secret = API_SECRET
+
+	timestamp = Time.now.to_i.to_s
+	method = "POST"
+	uri = URI.parse("https://api.bitflyer.com")
+	uri.path = "/v1/me/sendchildorder"
+	# 買い取引の際の詳細情報を指定
+	body = '{
+	  "product_code": "BTC_JPY",
+	  "child_order_type": "LIMIT",
+	  "side": "BUY",
+	  "price":' + price + ',
+	  "size": 0.001,
+	  "minute_to_expire": 10000,
+	  "time_in_force": "GTC"
+	}'
+
+	text = timestamp + method + uri.request_uri + body
+	sign = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new("sha256"), secret, text)
+
+	options = Net::HTTP::Post.new(uri.request_uri, initheader = {
+	  "ACCESS-KEY" => key,
+	  "ACCESS-TIMESTAMP" => timestamp,
+	  "ACCESS-SIGN" => sign,
+	  "Content-Type" => "application/json"
+	});
+	options.body = body
+
+	https = Net::HTTP.new(uri.host, uri.port)
+	https.use_ssl = true
+	response = https.request(options)
+	puts response.body
+end
