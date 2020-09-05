a77f58f7b08687c68999d604c885bd9dc0b19605
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-09-05 03:07:20 +0000

    first commit

---
+++ bitcoin.rb
@@ -0,0 +1,19 @@
+# ライブラリを指定
+require 'net/http'
+require 'uri'
+require 'json'
+require './key.rb'
+
+# 板情報を取得
+uri = URI.parse("https://api.bitflyer.com")
+uri.path = '/v1/getboard'
+uri.query = ""
+
+# データを取得
+https = Net::HTTP.new(uri.host, uri.port)
+https.use_ssl = true
+response = https.get uri.request_uri
+puts response.body
+
+response_hash = JSON.parse(response.body)
+puts response_hash["mid_price"]
